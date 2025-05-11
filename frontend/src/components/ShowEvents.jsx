bd12c373ba88c073d5997c51dfca046861cdaee5
Babu labana <115482429+babulabana@users.noreply.github.com>
2025-05-11 11:57:11 +0530

    kjb

--- frontend/src/components/ShowEvents.jsx
+++ frontend/src/components/ShowEvents.jsx
@@ -37,6 +37,8 @@ export default function ShowEvents() {
     })();
   }, []);
 
+  
+
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-3xl font-bold text-center mb-8">{cname}</h1>
